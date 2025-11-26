5b9ef4ca3a924d63c78969b22093b1630025f518
vivianemenezes571 <vivianemenezes5711@gmail.com>
2025-11-26 20:07:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Medidas");
+double m, cm, km;
+Console.Write("Escreva a medida (em metros): ");
+m = Convert.ToDouble(Console.ReadLine());
+
+cm = m * 100;
+km = m / 1000;
+
+Console.WriteLine("\n--- Equivalência ---");
+Console.WriteLine($"{cm} cm");
+Console.WriteLine($"{m} m");
+Console.WriteLine($"{km} Km");
